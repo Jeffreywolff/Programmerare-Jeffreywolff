54d26ecc6e7885cf89677c04078a812c3da886dc
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2019-09-09 11:41:39 +0200

    Nr.3 v37

--- Hemläxor/v.37/basics.java
+++ Hemläxor/v.37/basics.java
@@ -4,6 +4,8 @@ public class basics {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		System.out.println("Hello There! What's your name?");
+		String Name = input.nextLine();
 		
 	}
 
