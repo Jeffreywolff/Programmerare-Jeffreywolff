3e2e5634c69975aeefb2a9690b2f950915534c32
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2019-09-12 10:51:59 +0200

    Commit 14 v.37

--- Hemläxor/v.37/basics.java
+++ Hemläxor/v.37/basics.java
@@ -4,11 +4,22 @@ public class basics {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Hello There! What's your name?");
 		String Name = input.nextLine();
 		System.out.println( Name + ", what a beutifull name!");
 		System.out.println(Name + ", whats your age?");
 		String age = input.nextLine();
+		
+		System.out.println("Okey, Whats you home address?");
+		String address = input.nextLine();
+		System.out.println("Whats your postcode?");
+		String pcode = input.nextLine();
+		System.out.println("What city do you live in?");
+		String city = input.nextLine();
+		System.out.println("Whats your phone number?");
+		String phonenum = input.nextLine();
+		
 		
 	}
 
