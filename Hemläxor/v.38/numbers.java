7626b46a31747a0d622429eced0eba9dab5cc951
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2019-09-19 11:04:16 +0200

    v38 For loop

---
+++ Hemläxor/v.38/numbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class numbers {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int high = 0;
+		int low = 0;
+		int total = 0;
+		for(int a=0; a<10; a++) {
+			System.out.println("Type a number:");
+			int answer = input.nextInt();
+			if (a == 0) {
+			low = answer;
+			high = answer;
+
+		
+		
+		
+
+	}
+
+}
