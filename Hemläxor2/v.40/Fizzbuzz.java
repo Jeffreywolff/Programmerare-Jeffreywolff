65b9966786d12e1ddb558b36cd897ccf9e0876d9
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-10-05 11:57:08 +0200

    v.40 Fizzbuzz
    No comments allowed in kattis

--- Hemläxor2/v.40/Fizzbuzz.java
+++ Hemläxor2/v.40/Fizzbuzz.java
@@ -9,19 +9,19 @@ public class Fizzbuzz {
 		int N = input.nextInt();
 		
 		for(int a=1;a<=N;a++) {
-			//Kollar först om talet modulus X och Y är sant.
+			
 			 if(a%Y==0 && a%X==0) {
 				 System.out.println("FizzBuzz");
 			 }
-			 //Därefter om inte det första stämmer så kollar den om X är sant.
+			 
 			 else if(a%X==0) {
 				 System.out.println("Fizz");
 			 }
-			//Om inte x är sant så kollar den om Y är sant.
+			
 			 else if(a%Y==0) {
 				 System.out.println("Buzz");
 			 }
-			 // Om inget är sant så skriver outputen bara ut siffran a.
+			 
 			 else {
 				 System.out.println(a);
 			 }
