48752bc16430aec916b7ca1432fae87b6d99543a
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-11-11 11:24:26 +0100

    Uppgift 3 & 4.

--- PhysicsLibrary V.45/src/enums/main.java
+++ PhysicsLibrary V.45/src/enums/main.java
@@ -5,7 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println(fahrenheitToCelsius(50)); 
 		System.out.println(kelvinToCelsius(0));
-
+		System.out.println(fluidPressure(FluidTable.WATER, 10));
+		System.out.println(pressureUnderWater(10));
+		
 	}
 
 	public static double fahrenheitToCelsius(double fahrenheit) {
@@ -16,5 +18,19 @@ public class main {
 	public static double kelvinToCelsius(double kelvin) {
 		double celsius = (kelvin - 273.15);
 		return celsius;
+		// return kelvin - 273.15
 	}
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		//p*g*h
+		double pressure = (fluid.density * 9.82 * deep);
+		return pressure;
+	}
+	
+	public static double pressureUnderWater(double deep) {
+		//p*g*h
+		double pressure = FluidTable.WATER.density * 9.82 * deep;
+		return pressure;
+		
+	}
+	
 }
