0b2d82f22cedc9b1440f4a7a3c86bc36127d5976
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-11-30 13:09:38 +0100

    Update SolidTable.java

--- PhysicsLibrary V.45/src/enums/SolidTable.java
+++ PhysicsLibrary V.45/src/enums/SolidTable.java
@@ -3,7 +3,11 @@ package enums;
 public enum SolidTable {
 
 	ICE(0.92, 2.2, 0, 334),
-	IRON(7.87, 0.45, 1538, 276)
+	IRON(7.87, 0.45, 1538, 276),
+	GOLD(19.3, 0.129, 1064.18, 63),
+	SILICON(2.3, 0.71, 1410, 165),
+    SULFUR(2.1, 0.74, 120, 38),
+    NAPHTHALENE(1.15, 1.3, 80, 150),
 	;
 
 	double density;
