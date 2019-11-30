3008a02a60f0f2d5f1f8bbe31f8b9627ee8fcfac
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-11-30 13:12:54 +0100

    Update GasTable.java

--- PhysicsLibrary V.45/src/enums/GasTable.java
+++ PhysicsLibrary V.45/src/enums/GasTable.java
@@ -4,7 +4,9 @@ public enum GasTable{
 
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
+	OXYGEN(1.43, 0.92),
+    C3H8(2.02,1.53), //Propan
+    He(0.178, 5.2), //Helium
 	;
 
 	double density;
