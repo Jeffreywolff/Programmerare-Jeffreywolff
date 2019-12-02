91f6ad559510a79a9627b2c867c772c0b300224b
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-12-02 10:11:48 +0100

    Test intellj commit

---
+++ IntellJ-Programmering/main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int fabian = 17;
+        System.out.println("How old are you?");
+
+
+    }
+
+}
