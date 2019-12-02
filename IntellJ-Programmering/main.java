899b4f3899e10f9df7d6b2d72bd09cfdd20bb05f
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2019-12-02 10:36:01 +0100

    Arrayer

--- IntellJ-Programmering/main.java
+++ IntellJ-Programmering/main.java
@@ -6,10 +6,12 @@ public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int fabian = 17;
-        System.out.println("How old are you?");
+        int[] fiveTable = new int[20];
 
-
+        for (int i = 0; i < 20; i++) {
+            fiveTable[i] = 5 * i;
+            System.out.println(fiveTable[i]);
+        }
     }
 
 }
